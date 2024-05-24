8edb088384be6a8e7731db5ee31e756a6d9c2b78
Michael T. Hope <140920291+Mikehope254@users.noreply.github.com>
2024-05-24 03:02:51 -0700

    Update index.js
    const body = document.getElementByTagName("body")[0]
    //[0] is irrelevant because a valid HTML has one body

--- Color Flipper/index.js
+++ Color Flipper/index.js
@@ -1,14 +1,19 @@
-const body = document.getElementsByTagName("body")[0]
+const body = document.getElementsByTagName("body")
 
+//function for the red, blue and green buttons and colors
 function setColor(name) {
     body.style.backgroundColor = name;
 }
 
+//function for the random button and colors
 function randomColor(){
     const red = Math.round(Math.random() * 255)
     const green = Math.round(Math.random() * 255)
     const blue = Math.round(Math.random() * 255)
 
+//combination of red green and blue random colors from declared variables in the function
     const color = `rgb(${red},${green},${blue})`
+
+//Background color of the body will be changed because of the declared variable 'color'
     body.style.backgroundColor = color
 }
